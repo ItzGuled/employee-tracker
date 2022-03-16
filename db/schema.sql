358cd124114a3f549ac5bff4c1e7a6558f51391b
itzguled <itzguled55337@gmail.com>
2022-03-16 18:51:10 -0500

    seeds/connection

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREAT TABLE roles (
+ id INTEGER AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(30) NOT NULL,
+ dept_id INTEGER,
+ salary DECIMAL,
+ CONSTRAINT  fk_dept FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+   first_name VARCHAR(30),
+   last_name VARCHAR(30),
+   role_id INTEGER,
+   manager_id INTEGER,
+   CONSTRAINT  fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
